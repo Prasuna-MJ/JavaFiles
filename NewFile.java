e77175820e4ac10b92b14189cf8aa3e1203c5afb
prasuna <mjprasuna07@gmail.com>
2023-11-16 16:39:08 +0530

    Added

--- NewFile.java
+++ NewFile.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class NewFile {
+    public static void main(String[] args) {
+
+        System.out.println("How are you ........");
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Please Enter ur name");
+            String a = s.nextLine();
+            System.out.println(a);
+        }
+
+    }
+}
